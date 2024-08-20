c1678743f80c44476f864dc65f2611d71a707987
codesyariah122 <pujiermanto@gmail.com>
2024-08-20 22:24:55 +0700

    update lagi

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -2,6 +2,7 @@ import { createClient } from 'contentful';
 
 console.log('Contentful Space:', process.env.CONTENTFUL_SPACE);
 console.log('Contentful Access Token:', process.env.CONTENTFUL_ACCESS_TOKEN);
+console.log("Ok")
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE,
